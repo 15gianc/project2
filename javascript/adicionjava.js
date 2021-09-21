ccfeb02d8e09ad1ac6f3179f6fc539df576136c4
15gianc <giancarloodreman@gmail.com>
2021-09-21 18:57:54 -0500

    primer commit

---
+++ javascript/adicionjava.js
@@ -0,0 +1,53 @@
+
+/*alert("Bienvenido");
+
+prompt("Ingresa tu nombre");
+
+confirm("¿Quieres salir de la pag?")*/
+
+//funciones flecha
+
+const primeralbum = () =>{
+    let album1 = "X100pre";
+    alert("El album uno se llama: " + album1);
+
+
+}
+
+const segundoalbum = ()=>{  //no es necesario el let con el alert puedo mostrar todo
+    let album2 = "Oasis";
+    alert("El album dos se llama: " + album2);
+}
+
+const terceralbum = ()=>{
+    let album3 = "Up-Next";
+    alert("El album tres se llama: " + album3);
+}
+
+const cuartoalbum = ()=>{
+    let album4 = "YHLQMDLG";
+    alert("El album cuatro se llama: " + album4);
+}
+
+const quintoalbum = ()=>{
+    let album5 = "Las que no iban a salir";
+    alert("El album cinco se llama: " + album5);
+}
+
+const sextoalbum = ()=>{
+    let album6 = "El ultimo tour del mundo";
+    alert("El album seis se llama: " + album6);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Hola Desarrollador");
